facb3169c354e70ea8329b5ce212429a9e472640
TheBigerGamer Oficial <thebigergameroficial@gmail.com>
2019-10-22 14:27:44 +0100

    Update main.js

--- discord-bot-sourcefiles/main.js
+++ discord-bot-sourcefiles/main.js
@@ -14,30 +14,50 @@ const now = require("performance-now");
 const discord = require("discord.js");
 const bot = new discord.Client();
 bot.commands = new discord.Collection();
+bot.aliases = new discord.Collection();
 
 // --== File Manager (Command Handler) ==--
 
-fs.readdir("./discord-bot-sourcefiles/commands/", (err, files) => {
+const load = (dir = "./commands/") => {
 
-    if (err) console.log(err);
+	readdirSync(dir).forEach(dirs => {
+	// we read the commands directory for sub folders and filter the files with name with extension .js
+		const commands = readdirSync(`${dir}${sep}${dirs}${sep}`).filter(files => files.endsWith(".js"));
 
-    var jsFiles = files.filter(f => f.split(".").pop() === "js");
+		// we use for loop in order to get all the commands in sub directory
+		for (const file of commands) {
+		// We make a pull to that file so we can add it the bot.commands collection
+			const pull = require(`${dir}/${dirs}/${file}`);
+			// we check here if the command name or command category is a string or not or check if they exist
+			if (pull.help && typeof (pull.help.name) === "string" && typeof (pull.help.category) === "string") {
+				if (bot.commands.get(pull.help.name)) return console.warn(`${warning} Two or more commands have the same name ${pull.help.name}.`);
+				// we add the the comamnd to the collection, Map.prototype.set() for more info
+				bot.commands.set(pull.help.name, pull);
+				// we log if the command was loaded.
+				console.log(`${success} Loaded command ${pull.help.name}.`);
 
-    if (jsFiles.length <= 0) {
-        console.log("Kon geen files vinden!");
-        return;
-    }
+			}
+			else {
+			// we check if the command is loaded else throw a error saying there was command it didn't load
+				console.log(`${error} Error loading command in ${dir}${dirs}. you have a missing help.name or help.name is not a string. or you have a missing help.category or help.category is not a string`);
+				// we use continue to load other commands or else it will stop here
+				continue;
+			}
+			// we check if the command has aliases, is so we add it to the collection
+			if (pull.help.aliases && typeof (pull.help.aliases) === "object") {
+				pull.help.aliases.forEach(alias => {
+					// we check if there is a conflict with any other aliases which have same name
+					if (bot.aliases.get(alias)) return console.warn(`${warning} Two commands or more commands have the same aliases ${alias}`);
+					bot.aliases.set(alias, pull.help.name);
+				});
+			}
+		}
 
-    jsFiles.forEach((f, i) => {
+	});
+};
 
-        var fileGet = require(`./commands/${f}`);
-        console.log(`De file ${f} is geladen!`);
-
-        bot.commands.set(fileGet.help.name, fileGet);
-
-    })
-
-});
+// we call the function to all the commands.
+load();
 
 const chalk = require('chalk');
 
@@ -65,42 +85,34 @@ client.on('ready', () => {
 
 // Executed when message event
 client.on('message', async message => {
-    let commands = botCommands;
-    if (message.author.bot) return;
+    const prefix = bot.config.prefix;
+	const args = message.content.slice(prefix.length).trim().split(/ +/g);
+	const cmd = args.shift().toLowerCase();
 
-    let sender = message.author;
-    let senderUsername = sender.username;
-    let senderID = sender.id;
-    let content = message.content;
+	let command;
 
-    // --== Prefix en Bot Tag ==---
+	if (message.author.bot || !message.guild) return;
 
-    var prefixes = JSON.parse(fs.readFileSync("./prefixes.json"));
+	// If the message.member is uncached, message.member might return null.
+	// This prevents that from happening.
+	// eslint-disable-next-line require-atomic-updates
+	if (!message.member) message.member = await message.guild.fetchMember(message.author);
 
-    if (!prefixes[message.guild.id]) {
-        prefixes[message.guild.id] = {
-            prefixes: config.prefix
-        };
-    }
+	if (!message.content.startsWith(prefix)) return;
 
-    var prefix = prefixes[message.guild.id].prefixes;
+	if (cmd.length === 0) return;
+	if (bot.commands.has(cmd)) command = bot.commands.get(cmd);
+	else if (bot.aliases.has(cmd)) command = bot.commands.get(bot.aliases.get(cmd));
 
+	if (command) command.run(bot, message, args);
+	
     if(message.channel.type === "dm"){
         let time = Date.now();
         app.dmNotification(senderUsername, content, time);
     }
-
-    // ---=== Commands ===---
-
-    if(!message.content.startsWith(prefix)) return;
-    var messageArray = message.content.split(" ");
-    var command = messageArray[0];
-    var arguments = messageArray.slice(1);
-    var commands_getter = bot.commands.get(command.slice(prefix.length));
-    if (commands_getter) commands_getter.run(bot, message, arguments, prefix);
 });
 
-client.login(process.env.TOKEN);
+client.login(config.token);
 
 /**
  * Set a game status for the bot.
